a92d2e15a10fd6ffcffc2bd5cd00d12722baa1ba
Jesus0204 <a01705442@tec.mx>
2024-09-13 11:12:00 -0600

    Documentacion de funciones grid, drawx, drawo y floor

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,32 +1,76 @@
-from turtle import *
+from turtle import (update, up, goto, down, circle, setup,
+                    hideturtle, tracer, done, onscreenclick)
 
 from freegames import line
 
+"""
+Dibuja la cuadricula del juego.
+
+Parámetros:
+    Ninguno.
+
+Valor de Retorno:
+    Ninguno.
+"""
+
 
 def grid():
-    """Draw tic-tac-toe grid."""
     line(-67, 200, -67, -200)
     line(67, 200, 67, -200)
     line(-200, -67, 200, -67)
     line(-200, 67, 200, 67)
 
 
+"""
+Dibuja una 'X' en la cuadricula del juego.
+
+Parámetros:
+    x (int): Coordenada de eje X de la celda donde se dibujará la 'X'.
+    y (int): Coordenada eje Y de la celda donde se dibujará la 'X'.
+
+Valor de Retorno:
+    Ninguno.
+"""
+
+
 def drawx(x, y):
-    """Draw X player."""
     line(x, y, x + 133, y + 133)
     line(x, y + 133, x + 133, y)
 
 
+"""
+Dibuja una 'O' en la cuadricula del juego.
+
+Parámetros:
+    x (int): Coordenada de eje X de la celda donde se dibujará la 'O'.
+    y (int): Coordenada eje Y de la celda donde se dibujará la 'O'.
+
+Valor de Retorno:
+    Ninguno.
+"""
+
+
 def drawo(x, y):
-    """Draw O player."""
     up()
     goto(x + 67, y + 5)
     down()
     circle(62)
 
 
+"""
+Redondea un valor hacia abajo al inicio de la celda de la cuadricula
+con tamaño de 133 unidades.
+
+Parámetros:
+    value (int): Coordenada (eje X o Y) que se desea redondear hacia abajo.
+
+Valor de Retorno:
+    int: Coordenada redondeada hacia abajo al inicio de la celda
+    correspondiente en la cuadricula.
+"""
+
+
 def floor(value):
-    """Round value down to grid with square size 133."""
     return ((value + 200) // 133) * 133 - 200
 
 
